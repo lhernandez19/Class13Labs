3db5146db160afec0e3283a945bc0a719bb71b41
Luis Diego Hernandez <diegotapia1987@gmail.com>
2020-12-02 21:18:32 -0600

    new2

--- code.js
+++ code.js
@@ -1,11 +1,16 @@
+var studentsArray = [];
+
 $(document).ready(function(){
 
     //events
     $("#submit").click(results);
+    $("#sortName").click(sortName)
+    $("#sortPercentage").click(sortPercentage)
 
     //functions
 
-    function results (event){
+    function results (event)
+    {
         event.preventDefault();
 
         var myPoints =
@@ -13,7 +18,7 @@ $(document).ready(function(){
             firstName: "",
             lastName: "",
             pointsEarned: 0,
-            pointsPossible: 0
+            pointsPossible: 0,
         };
     
         myPoints.firstName = $("#fname").val();
@@ -25,6 +30,8 @@ $(document).ready(function(){
         var calculation = Math.round(myPoints.pointsEarned / myPoints.pointsPossible * 100);
 
         $("#resultsOutput").text(` ${myPoints.firstName} ${myPoints.lastName} result is: ${calculation.toFixed()}%`);
+
+        myPoints.calculation = calculation;
 
         if(calculation < 80)
         {
@@ -38,7 +45,83 @@ $(document).ready(function(){
         {
             $("#letterOutput").text("A");
         }
+
+
+        studentsArray.push(myPoints);
+
+        var nameOutput = "";
+        
+        for(var studentInfo of studentsArray)
+        {
+            nameOutput += studentInfo.firstName;
+        }
+
+        $("#nameOutput").text(nameOutput);
+
+        var percentageOutput = "";
+        
+        for(var studentInfo of studentsArray)
+        {
+            percentageOutput += studentInfo.calculation;
+        }
+
+        $("#percentageOutput").text(percentageOutput);
+       
     }
 
+
+    function sortName ()
+    {
+        studentsArray.sort(function(a, b)
+        {
+            if (a.firstName < b.firstName)
+            {
+                return -1;
+            }
+            else if (a.firstName > b.firstName)
+            {
+                return 1;
+            }
+            else 
+            {
+                return 0;
+            }
+        });
+        var nameOutput = "";
+        
+        for(var studentInfo of studentsArray)
+        {
+            nameOutput += studentInfo.firstName;
+        }
+
+        $("#nameOutput").text(nameOutput);
+    }
+
+    function sortPercentage ()
+    {
+        studentsArray.sort(function(a, b)
+        {
+            if (a.calculation < b.calculation)
+            {
+                return -1;
+            }
+            else if (a.calculation > b.calculation)
+            {
+                return 1;
+            }
+            else 
+            {
+                return 0;
+            }
+        });
+        var percentageOutput = "";
+        
+        for(var studentInfo of studentsArray)
+        {
+            percentageOutput += studentInfo.calculation;
+        }
+
+        $("#percentageOutput").text(percentageOutput);
+    }
 
 });
